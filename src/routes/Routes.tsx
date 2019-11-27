7686ea2aa74a3273c3d03c7256c65839adda3ea9
杨振明 <yangzhenming@yangzheingdeMBP.lan>
2019-11-27 23:43:21 +0800

    1.添加图标插件

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
+
 import { createAppContainer } from 'react-navigation';
 
-
 import { createStackNavigator, } from 'react-navigation-stack';
-
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { HomeScreen } from './HomeScreen';
+//@ts-ignore
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+import { HomeScreen } from './HomeScreen';
 import { AboutScreen } from './About';
 
 
@@ -47,9 +49,23 @@ AppNavigator.navigationOptions = {
     tabBarLabel: 'Home!',
 };
 
-AppBNavigator.navigationOptions = {
-    tabBarLabel: 'Settings!',
-};
+AppBNavigator.navigationOptions = ({ navigation }: any) => ({
+    tabBarIcon: ({ focused, horizontal, tintColor }: any) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'About') {
+            iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Settings') {
+            iconName = `ios-options`;
+        } else {
+            iconName = `ios-options`;
+        }
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+    },
+    tabBarLabel: 'About!',
+});
 
 
 
